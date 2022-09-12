0e967f03697f6c78dea169283d0545cc96a501c0
nwuensche <gh@nwuensche.com>
2022-09-12 20:49:51 +0200

    Add more Extensions functions

--- mylibrary/src/main/java/de/nwuensche/mylibrary/AppAuthExtensions.kt
+++ mylibrary/src/main/java/de/nwuensche/mylibrary/AppAuthExtensions.kt
@@ -1,7 +1,7 @@
 package de.nwuensche.mylibrary
 
 import android.net.Uri
-import net.openid.appauth.AuthorizationServiceConfiguration
+import net.openid.appauth.*
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
@@ -16,6 +16,80 @@ suspend fun fetchFromIssuer(openIdConnectIssuerUri: Uri): AuthorizationServiceCo
             cont.resumeWithException(ex)
         } else {
             cont.resumeWithException(IllegalStateException("serviceConfiguration AND exception is null")) //fetchFromIssuer-Dok says exactly one is non-null
+        }
+    }
+}
+
+suspend fun fetchFromUrl(openIdConnectIssuerUri: Uri): AuthorizationServiceConfiguration = suspendCoroutine { cont ->
+    AuthorizationServiceConfiguration.fetchFromIssuer(
+        openIdConnectIssuerUri
+    ) { serviceConfiguration, ex ->
+        if (serviceConfiguration != null) {
+            cont.resume(serviceConfiguration)
+        } else if (ex != null) {
+            cont.resumeWithException(ex)
+        } else {
+            cont.resumeWithException(IllegalStateException("serviceConfiguration AND exception is null")) //fetchFromIssuer-Dok says exactly one is non-null
+        }
+    }
+}
+
+suspend fun AuthorizationService.performTokenRequest(tokenRequest: TokenRequest): TokenResponse = suspendCoroutine { cont ->
+    this.performTokenRequest(
+        tokenRequest
+    ) { response, ex ->
+        if (response != null) {
+            cont.resume(response)
+        } else if (ex != null) {
+            cont.resumeWithException(ex)
+        } else {
+            cont.resumeWithException(IllegalStateException("response AND exception is null")) //performTokenRequest-Dok says exactly one is non-null
+        }
+    }
+}
+
+suspend fun AuthorizationService.performTokenRequest(tokenRequest: TokenRequest, clientAuthentication: ClientAuthentication): TokenResponse = suspendCoroutine { cont ->
+    this.performTokenRequest(
+        tokenRequest,
+        clientAuthentication
+    ) { response, ex ->
+        if (response != null) {
+            cont.resume(response)
+        } else if (ex != null) {
+            cont.resumeWithException(ex)
+        } else {
+            cont.resumeWithException(IllegalStateException("response AND exception is null")) //performTokenRequest-Dok says exactly one is non-null
+        }
+    }
+}
+
+data class FreshTokens(val accessToken: String?, val idToken: String?)
+
+suspend fun AuthState.performActionWithFreshTokens(service: AuthorizationService): FreshTokens = suspendCoroutine { cont ->
+    this.performActionWithFreshTokens(
+        service
+    ) { accessToken, idToken, ex ->
+        if (accessToken != null || idToken != null) {
+            cont.resume(FreshTokens(accessToken, idToken))
+        } else if (ex != null) {
+            cont.resumeWithException(ex)
+        } else {
+            cont.resumeWithException(IllegalStateException("tokens AND exception is null")) //performActionWithFreshTokens-Dok says exactly one is non-null
+        }
+    }
+}
+
+suspend fun AuthState.performActionWithFreshTokens(service: AuthorizationService, clientAuthentication: ClientAuthentication): FreshTokens = suspendCoroutine { cont ->
+    this.performActionWithFreshTokens(
+        service,
+        clientAuthentication
+    ) { accessToken, idToken, ex ->
+        if (accessToken != null || idToken != null) {
+            cont.resume(FreshTokens(accessToken, idToken))
+        } else if (ex != null) {
+            cont.resumeWithException(ex)
+        } else {
+            cont.resumeWithException(IllegalStateException("tokens AND exception is null")) //performActionWithFreshTokens-Dok says exactly one is non-null
         }
     }
 }
